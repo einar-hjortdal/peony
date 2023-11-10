84993165c7b5f123cba2e68f42a1d97583efbc72
Coahconko <info@coachonko.com>
2023-11-10 13:38:09 +0100

    use send_error

--- src/storefront_posts.v
+++ src/storefront_posts.v
@@ -2,43 +2,30 @@ module main
 
 import vweb
 import models
-import utils
 
 ['/storefront/posts'; get]
 pub fn (mut app App) storefront_posts_get() vweb.Result {
-	posts := models.post_list(mut app.db, 'post') or {
-		app.logger.debug(err.msg())
-		app.set_status(500, 'Internal server error')
-		peony_error := utils.new_peony_error(500, err.msg())
-		return app.json(peony_error)
-	}
+	fn_name := 'storefront_posts_get'
+
+	posts := models.post_list(mut app.db, 'post') or { return app.send_error(err, fn_name) }
 	return app.json(posts)
 }
 
 // TODO maybe ID does not exist
 ['/storefront/posts/:id'; get]
 pub fn (mut app App) storefront_post_get_by_id(id string) vweb.Result {
-	posts := models.post_retrieve_by_id(mut app.db, id) or {
-		app.logger.debug(err.msg())
-		app.set_status(500, 'Internal server error')
-		peony_error := utils.new_peony_error(500, err.msg())
-		return app.json(peony_error)
-	}
+	fn_name := 'storefront_post_get_by_id'
+
+	posts := models.post_retrieve_by_id(mut app.db, id) or { return app.send_error(err, fn_name) }
 	return app.json(posts)
 }
 
 ['/storefront/posts/handle/:handle'; get]
 fn (mut app App) storefront_post_get_by_handle(handle string) vweb.Result {
+	fn_name := 'storefront_post_get_by_handle'
+
 	post := models.post_retrieve_by_handle(mut app.db, handle) or {
-		if err is utils.PeonyError {
-			app.logger.debug(err.to_string())
-			app.set_status(404, err.data())
-			return app.json(err)
-		}
-		app.logger.debug(err.msg())
-		app.set_status(500, 'Internal server error')
-		peony_error := utils.new_peony_error(500, err.msg())
-		return app.json(peony_error)
+		return app.send_error(err, fn_name)
 	}
 	return app.json(post)
 }
@@ -51,11 +38,8 @@ fn (mut app App) storefront_post_get_by_handle(handle string) vweb.Result {
 
 ['/storefront/pages'; get]
 pub fn (mut app App) storefront_pages_get() vweb.Result {
-	pages := models.post_list(mut app.db, 'page') or {
-		app.logger.debug(err.msg())
-		app.set_status(500, 'Internal server error')
-		peony_error := utils.new_peony_error(500, err.msg())
-		return app.json(peony_error)
-	}
+	fn_name := 'storefront_pages_get'
+
+	pages := models.post_list(mut app.db, 'page') or { return app.send_error(err, fn_name) }
 	return app.json(pages)
 }
