a88bc74b75a7a8938e3a6448f8cafc9c47cafd66
Coahconko <info@coachonko.com>
2023-12-11 16:14:10 +0100

    Remove /storefront/pages/handle/:handle endpoint

--- src/storefront_posts.v
+++ src/storefront_posts.v
@@ -7,10 +7,8 @@ import strconv
 import models
 import utils
 
-// TODO instead of route to get by handle, use /storefront/posts with filter_handle param
-
 @['/storefront/posts'; get]
-pub fn (mut app App) storefront_posts_get() vweb.Result {
+fn (mut app App) storefront_posts_get() vweb.Result {
 	fn_name := 'storefront_posts_get'
 
 	mut q_include_authors := false
@@ -64,21 +62,11 @@ pub fn (mut app App) storefront_posts_get() vweb.Result {
 
 // TODO maybe ID does not exist
 @['/storefront/posts/:id'; get]
-pub fn (mut app App) storefront_post_get_by_id(id string) vweb.Result {
+fn (mut app App) storefront_post_get_by_id(id string) vweb.Result {
 	fn_name := 'storefront_post_get_by_id'
 
 	posts := models.post_retrieve_by_id(mut app.db, id) or { return app.send_error(err, fn_name) }
 	return app.json(posts)
-}
-
-@['/storefront/posts/handle/:handle'; get]
-pub fn (mut app App) storefront_post_post_get_by_handle(handle string) vweb.Result {
-	fn_name := 'storefront_post_get_by_handle'
-
-	post := models.post_retrieve_by_handle(mut app.db, handle) or {
-		return app.send_error(err, fn_name)
-	}
-	return app.json(post)
 }
 
 /*
@@ -87,8 +75,9 @@ pub fn (mut app App) storefront_post_post_get_by_handle(handle string) vweb.Resu
 *
 */
 
+// TODO abstract storefront_posts_get to another function, use same as storefront_posts_get
 @['/storefront/pages'; get]
-pub fn (mut app App) storefront_pages_get() vweb.Result {
+fn (mut app App) storefront_pages_get() vweb.Result {
 	fn_name := 'storefront_pages_get'
 
 	params := models.PostListParams{
@@ -100,43 +89,4 @@ pub fn (mut app App) storefront_pages_get() vweb.Result {
 
 	pages := models.post_list(mut app.db, params) or { return app.send_error(err, fn_name) }
 	return app.json(pages)
-}
-
-@['/storefront/pages/handle/:handle'; get]
-pub fn (mut app App) storefront_post_page_get_by_handle(handle string) vweb.Result {
-	fn_name := 'storefront_post_get_by_handle'
-
-	mut q_include_authors := false
-	if 'include_authors' in app.query {
-		if app.query['include_authors'] == '' {
-			err := utils.new_peony_error(400, 'empty include_authors query')
-			return app.send_error(err, fn_name)
-		} else {
-			if utils.can_parse_bool(app.query['include_authors']) {
-				q_include_authors = utils.parse_bool(app.query['include_authors'])
-			} else {
-				err := utils.new_peony_error(400, 'include_authors query cannot be parsed to a boolean value')
-				return app.send_error(err, fn_name)
-			}
-		}
-	}
-
-	p_filter_handle := [handle]
-
-	params := models.PostListParams{
-		include_authors: q_include_authors
-		filter_post_type: 'page'
-		filter_deleted: true
-		filter_handle: p_filter_handle
-	}
-
-	posts := models.post_list(mut app.db, params) or { return app.send_error(err, fn_name) }
-
-	if posts.len == 0 {
-		err := utils.new_peony_error(404, 'No page exists with the given handle')
-		return app.json(err)
-	}
-
-	post := posts[0]
-	return app.json(post)
 }
