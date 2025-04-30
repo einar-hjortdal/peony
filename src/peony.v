9d74242d538cfbd2a9db24c1d9c1d591d6b123af
Einar Hjortdal <einar.hjortdal@outlook.com>
2025-04-30 17:58:51 +0200

    Attempt setup session middleware

--- src/peony.v
+++ src/peony.v
@@ -7,19 +7,22 @@ import log
 // first party
 import einar_hjortdal.firebird
 import einar_hjortdal.luuid
-// import einar_hjortdal.sessions
+import einar_hjortdal.sessions
 
+@[heap]
 pub struct App {
+	veb.Middleware[Context]
 mut:
 	luuid_generator &luuid.Generator
 	fb              &firebird.Connection
+	session_store   &sessions.JsonWebTokenStore
 }
 
 pub struct Context {
 	veb.Context
+mut:
+	session sessions.Session
 }
-
-// TODO middlwares per-route: check auth where needed, attach user data to context.
 
 fn set_log_level() {
 	if os.getenv('DEBUG') == 'true' {
@@ -27,6 +30,11 @@ fn set_log_level() {
 	} else {
 		log.set_level(log.Level.info)
 	}
+}
+
+fn (mut app App) session_middleware(mut ctx Context) bool {
+	ctx.session = app.session_store.new(ctx.req, os.getenv('SESSION_NAME'))
+	return true
 }
 
 fn main() {
@@ -39,10 +47,18 @@ fn main() {
 
 	prepare_db(mut new_firebird_connection, mut new_luuid_generator) or { panic(err) }
 
+	mut session_store_options := sessions.JsonWebTokenStoreOptions{
+		secret: os.getenv('SESSION_SECRET')
+	}
+	mut session_store := sessions.new_jwt_store(mut session_store_options) or { panic(err) }
+
 	mut app := App{
 		luuid_generator: new_luuid_generator
 		fb:              new_firebird_connection
+		session_store:   session_store
 	}
+	app.route_use('/admin/:path...', handler: app.session_middleware)
+
 	port := os.getenv(env_port).int()
 	veb.run[App, Context](mut app, port)
 }
