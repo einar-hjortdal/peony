cd2afe8adc85c9989c7577d092da4d866425fa54
Einar Hjortdal <einar.hjortdal@outlook.com>
2025-06-28 10:08:20 +0200

    Fix presentation of locales

--- routes_admin_locales.v
+++ routes_admin_locales.v
@@ -6,12 +6,18 @@ import veb
 @['/admin/locales/'; get]
 fn (mut app App) admin_locales_get(mut ctx Context) veb.Result {
 	p := extract_retrieve_locales_params(ctx.query)
-	locales, count := app.retrieve_locales(p) or {
+	internal_locales, count := app.retrieve_locales(p) or {
 		ctx.res.set_status(http.Status.internal_server_error)
 		return ctx.json(new_peony_error('Could not retrieve locales from database', err.msg()))
 	}
+
+	mut external_locales := []LocaleResponse{len: internal_locales.len}
+	for i := 0; i < internal_locales.len; i++ {
+		external_locales[i] = format_locale_response(internal_locales[i])
+	}
+
 	r := ListResponse{
-		items:  locales
+		items:  external_locales
 		count:  count
 		offset: get_offset_amount(p.offset)
 		fetch:  get_fetch_amount(p.fetch)
