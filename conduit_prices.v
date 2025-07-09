5a254630743e0328712f065ee43bf700c91844a5
Einar Hjortdal <einar.hjortdal@outlook.com>
2025-07-09 11:39:07 +0200

    Progress on prices

--- conduit_prices.v
+++ conduit_prices.v
@@ -27,14 +27,42 @@ fn calculate_taxes() {
 
 fn is_original_price(ma MoneyAmount, currency_code string, region_id_bin []u8) bool {
 	return ma.min_quantity.is_null && ma.max_quantity.is_null
-		&& (region_id_bin.len == 0 || ma.region_id_bin == region_id_bin)
+		&& (region_id_bin.len == 0 || ma.region_id_bin.value == region_id_bin)
+}
+
+fn select_original_price(original_prices []MoneyAmount) MoneyAmount {
+	mut with_region := false
+	for i := 0; i < original_prices.len; i++ {
+		if !original_prices[i].region_id_bin.is_null {
+			with_region = true
+			break
+		}
+	}
+
+	mut lowest := original_prices[0]
+	if with_region {
+		for i := 0; i < original_prices.len; i++ {
+			if !original_prices[i].region_id_bin.is_null
+				&& original_prices[i].amount < lowest.amount {
+				lowest = original_prices[i]
+			}
+		}
+		return lowest
+	}
+
+	for i := 0; i < original_prices.len; i++ {
+		if original_prices[i].amount < lowest.amount {
+			lowest = original_prices[i]
+		}
+	}
+	return lowest
 }
 
 fn is_valid_price(ma MoneyAmount, quantity i32, currency_code string, region_id_bin []u8) bool {
 	return (ma.min_quantity.is_null || ma.max_quantity.value < quantity)
 		&& (ma.max_quantity.is_null || ma.max_quantity.value > quantity)
 		&& (currency_code == '' || ma.currency_code == currency_code)
-		&& (region_id_bin.len == 0 || ma.region_id_bin == region_id_bin)
+		&& (region_id_bin.len == 0 || ma.region_id_bin.value == region_id_bin)
 }
 
 // this function should find the lowest possible price that fits all the criteria.
@@ -45,11 +73,11 @@ fn is_valid_price(ma MoneyAmount, quantity i32, currency_code string, region_id_
 // product -> product type -> region (TODO verify)
 fn calculate_price(variant Variant, quantity i32, pctx PriceContext) Price {
 	// for now just consider variant.money_amounts and pctx.region
-	mut original_price := MoneyAmount{}
+	mut original_prices := []MoneyAmount{}
 	mut valid_money_amounts := []MoneyAmount{}
 	for i := 0; i < variant.money_amounts.len; i++ {
 		if is_original_price(variant.money_amounts[i], pctx.currency_code, pctx.region_id_bin) {
-			original_price = variant.money_amounts[i]
+			original_prices = arrays.concat(original_prices, variant.money_amounts[i])
 		}
 
 		if is_valid_price(variant.money_amounts[i], quantity, pctx.currency_code, pctx.region_id_bin) {
@@ -57,16 +85,17 @@ fn calculate_price(variant Variant, quantity i32, pctx PriceContext) Price {
 		}
 	}
 
-	mut calculated_price := original_price
-	for i := 0; i < valid_money_amounts.len; i++ {
-		if valid_money_amounts[i].amount < calculated_price.amount {
-			calculated_price = valid_money_amounts[i]
-		}
-	}
+	original_price := select_original_price(original_prices)
 
 	return Price{
-		original_price:   original_price.amount
-		calculated_price: calculated_price.amount
-		currency_code:    pctx.currency_code
+		currency_code:  pctx.currency_code
+		original_price: original_price.amount
+		// original_price_does_include_tax:
+		// original_price_excluding_tax:
+		// original_price_including_tax:
+		// calculated_price:
+		// calculated_price_does_include_tax:
+		// calculated_price_excluding_tax:
+		// calculated_price_including_tax:
 	}
 }
--- model_money_amount.v
+++ model_money_amount.v
@@ -7,49 +7,30 @@ struct MoneyAmount {
 	id_bin            []u8
 	created_at        firebird.DateTime
 	updated_at        firebird.DateTime
-	deleted_at        firebird.DateTime
+	deleted_at        firebird.NullDateTime
 	currency_code     string
 	amount            i32
 	min_quantity      firebird.NullI32
 	max_quantity      firebird.NullI32
-	price_list_id     string
-	price_list_id_bin []u8
-	region_id         string
-	region_id_bin     []u8
-	variant_id        string
-	variant_id_bin    []u8
+	price_list_id_bin firebird.NullArrayU8
+	region_id_bin     firebird.NullArrayU8
+	variant_id_bin    firebird.NullArrayU8
 }
 
 fn parse_money_amount(v []firebird.Value) !MoneyAmount {
 	id_bin, _ := v[0].get_array_u8()!
 	created_at, _ := v[1].get_date_time()!
 	updated_at, _ := v[2].get_date_time()!
-	deleted_at, _ := v[3].get_date_time()!
+	deleted_at := v[3].get_null_date_time()!
 	currency_code, _ := v[4].get_string()!
 	amount, _ := v[5].get_i32()!
 	min_quantity := v[6].get_null_i32()!
 	max_quantity := v[7].get_null_i32()!
-
-	price_list_id_bin, price_list_id_is_null := v[8].get_array_u8()!
-	region_id_bin, region_id_is_null := v[9].get_array_u8()!
-	variant_id_bin, variant_id_is_null := v[10].get_array_u8()!
+	price_list_id_bin := v[8].get_null_array_u8()!
+	region_id_bin := v[9].get_null_array_u8()!
+	variant_id_bin := v[10].get_null_array_u8()!
 
 	id := id_bin_to_string(id_bin)!
-	mut price_list_id := ''
-	mut region_id := ''
-	mut variant_id := ''
-
-	if !price_list_id_is_null {
-		price_list_id = id_bin_to_string(price_list_id_bin)!
-	}
-
-	if !region_id_is_null {
-		region_id = id_bin_to_string(region_id_bin)!
-	}
-
-	if !variant_id_is_null {
-		variant_id = id_bin_to_string(variant_id_bin)!
-	}
 
 	return MoneyAmount{
 		id:                id
@@ -61,11 +42,8 @@ fn parse_money_amount(v []firebird.Value) !MoneyAmount {
 		amount:            amount
 		min_quantity:      min_quantity
 		max_quantity:      max_quantity
-		price_list_id:     price_list_id
 		price_list_id_bin: price_list_id_bin
-		region_id:         region_id
 		region_id_bin:     region_id_bin
-		variant_id:        variant_id
 		variant_id_bin:    variant_id_bin
 	}
 }
