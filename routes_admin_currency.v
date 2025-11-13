6753caf4001620c13d8f928fcd7d6866d5e4d0b6
Einar Hjortdal <einar.hjortdal@outlook.com>
2025-11-13 09:46:07 +0100

    Add admin route

--- routes_admin_currency.v
+++ routes_admin_currency.v
@@ -16,3 +16,13 @@ pub fn (mut app App) admin_currencies_get(mut ctx Context) veb.Result {
 
 	return conduit_currency_list(mut app, mut ctx, p)
 }
+
+// get currency by code
+@['/admin/currencies/:code'; get]
+pub fn (mut app App) admin_currencies_get_by_code(mut ctx Context, code string) veb.Result {
+	if code.len != 3 {
+		return handle_error_400(mut ctx, error_id_invalid, 'currency code too long or too short')
+	}
+
+	return conduit_currency_get(mut app, mut ctx, code)
+}
