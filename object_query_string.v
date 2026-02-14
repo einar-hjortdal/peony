2b11dc4d29f6344218dbffaf052ece76361e9121
Einar Hjortdal <einar.hjortdal@outlook.com>
2026-02-14 17:03:46 +0100

    Remove useless files

--- object_query_string.v
+++ object_query_string.v
@@ -12,6 +12,35 @@ pub:
 	order        ZeroString
 }
 
+fn hygienise_user_list_request_query(m map[string]string) !UserListParams {
+	p := extract_user_list_request_query(m)
+
+	ids_bin := zero_array_id_string_to_array_id_bin(p.ids) or {
+		return new_error_bad_request(error_id_invalid, 'ids')
+	}
+
+	include_deleted := p.with_deleted.is_set && p.with_deleted.v
+
+	order_direction := get_order_direction(p.order) or {
+		return new_error_bad_request(error_order_direction_invalid, details_order_direction_invalid)
+	}
+
+	return UserListParams{
+		filter_by_id:        p.ids.is_set
+		ids_bin:             ids_bin
+		filter_by_email:     p.email.is_set
+		email:               p.email.v
+		filter_by_handle:    p.handle.is_set
+		handle:              p.handle.v
+		include_deleted:     include_deleted
+		use_offset:          p.offset.is_set
+		offset:              p.offset.v
+		fetch:               hygienise_fetch_amount(p.fetch)!
+		use_order_direction: p.order.is_set
+		order_direction:     order_direction
+	}
+}
+
 fn extract_user_list_request_query(m map[string]string) UserListRequestQuery {
 	return UserListRequestQuery{
 		ids:          zero_array_string(m, 'ids')
@@ -41,6 +70,30 @@ fn extract_region_list_request_query(m map[string]string) RegionListRequestQuery
 		offset:       zero_i32(m, 'offset')
 		fetch:        zero_i32(m, 'fetch')
 		order:        zero_string(m, 'order')
+	}
+}
+
+fn hygienise_region_list_request_query(p RegionListRequestQuery) !RegionRetriveParams {
+	ids_bin := zero_array_id_string_to_array_id_bin(p.ids) or {
+		return new_error_bad_request(error_id_invalid, 'ids')
+	}
+
+	include_deleted := p.with_deleted.is_set && p.with_deleted.v
+
+	order_direction := get_order_direction(p.order) or {
+		return new_error_bad_request(error_order_direction_invalid, details_order_direction_invalid)
+	}
+
+	return RegionRetriveParams{
+		filter_by_id:        p.ids.is_set
+		ids_bin:             ids_bin
+		filter_by_name:      p.name.is_set
+		include_deleted:     include_deleted
+		use_offset:          p.offset.is_set
+		offset:              p.offset.v
+		fetch:               hygienise_fetch_amount(p.fetch)!
+		use_order_direction: p.order.is_set
+		order_direction:     order_direction
 	}
 }
 
@@ -84,6 +137,21 @@ pub:
 	order  ZeroString
 }
 
+fn hygienise_retrieve_locale_params(m map[string]string) !LocaleRetrieveParamsHygienised {
+	ids := zero_array_string(m, 'ids')
+	ids_bin := zero_array_id_string_to_array_id_bin(ids) or {
+		return new_error_bad_request(error_id_invalid, 'ids')
+	}
+
+	return LocaleRetrieveParamsHygienised{
+		ids:     ids
+		ids_bin: ids_bin
+		offset:  zero_i32(m, 'offset')
+		fetch:   zero_i32(m, 'fetch')
+		order:   zero_string(m, 'order')
+	}
+}
+
 struct ListSalesChannelsParams {
 pub:
 	ids         ZeroArrayString
@@ -93,6 +161,16 @@ pub:
 	offset      ZeroI32
 	fetch       ZeroI32
 	order       ZeroString
+}
+
+struct ListSalesChannelsParamsHygienised {
+	ids             ZeroArrayString
+	ids_bin         [][]u8
+	product_ids     ZeroArrayString
+	product_ids_bin [][]u8
+	offset          ZeroI32
+	fetch           ZeroI32
+	order           ZeroString
 }
 
 fn extract_retrieve_sales_channels_params(p map[string]string) ListSalesChannelsParams {
@@ -112,6 +190,20 @@ pub:
 	allow_backorder ZeroBool
 	region_id       ZeroString
 	title           ZeroString
+	with_deleted    ZeroBool
+	offset          ZeroI32
+	fetch           ZeroI32
+	order           ZeroString
+}
+
+struct RetrieveProductVariantParamsHygienised {
+	ids             ZeroArrayString
+	ids_bin         [][]u8
+	product_ids     ZeroArrayString
+	product_ids_bin [][]u8
+	allow_backorder ZeroBool
+	region_id       ZeroString
+	region_id_bin   []u8
 	with_deleted    ZeroBool
 	offset          ZeroI32
 	fetch           ZeroI32
@@ -322,6 +414,26 @@ fn extract_category_get_request_params(m map[string]string) ProductCategoryGetRe
 	}
 }
 
+fn hygienise_category_get_request_query(p ProductCategoryGetRequestQuery, category_id_bin []u8) !CategoryRetrieveParams {
+	locale_id_bin := zero_id_string_to_id_bin(p.locale_id) or {
+		return new_error_bad_request(error_id_invalid, 'locale_id')
+	}
+
+	return CategoryRetrieveParams{
+		filter_by_id:  true
+		ids_bin:       [category_id_bin]
+		locale_id_bin: locale_id_bin
+	}
+}
+
+struct LocaleRetrieveParamsHygienised {
+	ids     ZeroArrayString
+	ids_bin [][]u8
+	offset  ZeroI32
+	fetch   ZeroI32
+	order   ZeroString
+}
+
 // handles expects a string that is a single handle, or many comma-separated handles.
 pub struct ProductCategoryListRequestQuery {
 pub:
@@ -351,6 +463,53 @@ fn extract_category_list_request_query(m map[string]string) ProductCategoryListR
 		fetch:               zero_i32(m, 'fetch')
 		order:               zero_string(m, 'order')
 		locale_id:           zero_string(m, 'locale_id')
+	}
+}
+
+// TODO return error if invalid sorting order
+fn hygienise_category_list_request_query(p ProductCategoryListRequestQuery) !CategoryRetrieveParams {
+	ids_bin := zero_array_id_string_to_array_id_bin(p.ids) or {
+		return new_error_bad_request(error_id_invalid, 'ids')
+	}
+
+	parent_category_ids_bin := zero_array_id_string_to_array_id_bin(p.parent_category_ids) or {
+		return new_error_bad_request(error_id_invalid, 'parent_category_ids')
+	}
+
+	product_ids_bin := zero_array_id_string_to_array_id_bin(p.product_ids) or {
+		return new_error_bad_request(error_id_invalid, 'product_ids')
+	}
+
+	include_deleted := p.with_deleted.is_set && p.with_deleted.v
+
+	locale_id_bin := zero_id_string_to_id_bin(p.locale_id) or {
+		return new_error_bad_request(error_id_invalid, 'locale_id')
+	}
+
+	order_direction := get_order_direction(p.order) or {
+		return new_error_bad_request(error_order_direction_invalid, details_order_direction_invalid)
+	}
+
+	return CategoryRetrieveParams{
+		filter_by_id:                  p.ids.is_set
+		ids_bin:                       ids_bin
+		filter_by_handle:              p.handles.is_set
+		handles:                       p.handles.v
+		filter_by_is_active:           p.is_active.is_set
+		is_active:                     p.is_active.v
+		filter_by_is_internal:         p.is_internal.is_set
+		is_internal:                   p.is_internal.v
+		filter_by_product_ids:         p.product_ids.is_set
+		product_ids_bin:               product_ids_bin
+		filter_by_parent_category_ids: p.parent_category_ids.is_set
+		parent_category_ids_bin:       parent_category_ids_bin
+		include_deleted:               include_deleted
+		locale_id_bin:                 locale_id_bin
+		use_offset:                    p.offset.is_set
+		offset:                        p.offset.v
+		fetch:                         hygienise_fetch_amount(p.fetch)!
+		use_order_direction:           p.order.is_set
+		order_direction:               order_direction
 	}
 }
 
--- utils.v
+++ utils.v
@@ -3,6 +3,10 @@ module peony
 import net.http
 import einar_hjortdal.luuid
 
+pub const min_fetch = i32(1)
+pub const max_fetch = i32(250)
+pub const default_offset = i32(0)
+
 const error_database_data_malformed = 'Data retrieved from database is malformed'
 const error_field_empty = 'Field cannot be empty'
 const error_field_too_long = 'Field too long'
@@ -17,6 +21,95 @@ const error_transaction_rollback = 'Failed to rollback transaction'
 const error_transaction_start = 'Failed to start transaction'
 
 const details_order_direction_invalid = 'order direction must either be ${order_direction_asc} or ${order_direction_desc}'
+
+struct ZeroString {
+	v      string
+	is_set bool
+}
+
+// TODO change `v` to `value`
+fn zero_string(m map[string]string, k string) ZeroString {
+	if k in m {
+		return ZeroString{
+			v:      m[k]
+			is_set: true
+		}
+	}
+	return ZeroString{}
+}
+
+struct ZeroArrayString {
+	v      []string
+	is_set bool
+}
+
+fn zero_array_string(m map[string]string, k string) ZeroArrayString {
+	s := zero_string(m, k)
+	if s.is_set {
+		return ZeroArrayString{
+			v:      s.v.split(',')
+			is_set: true
+		}
+	}
+	return ZeroArrayString{}
+}
+
+struct ZeroI32 {
+	v      i32
+	is_set bool
+}
+
+fn zero_i32(m map[string]string, k string) ZeroI32 {
+	s := zero_string(m, k)
+	if s.is_set {
+		return ZeroI32{
+			v:      s.v.i32()
+			is_set: true
+		}
+	}
+	return ZeroI32{}
+}
+
+struct ZeroBool {
+	v      bool
+	is_set bool
+}
+
+fn zero_bool(m map[string]string, k string) ZeroBool {
+	s := zero_string(m, k)
+	if s.is_set {
+		if s.v == '' {
+			return ZeroBool{
+				v:      true
+				is_set: true
+			}
+		}
+
+		return ZeroBool{
+			v:      parse_bool(s.v)
+			is_set: true
+		}
+	}
+	return ZeroBool{}
+}
+
+fn hygienise_fetch_amount(zi32 ZeroI32) !i32 {
+	if !zi32.is_set {
+		return max_fetch
+	}
+
+	if zi32.v < 1 {
+		return new_error_bad_request('Too few objects requested. Minimum ${min_fetch} must be requested',
+			'requested ${zi32.v}')
+	}
+
+	if zi32.v > max_fetch {
+		return new_error_bad_request('Too many objects requested. Maximum ${max_fetch} can be requested',
+			'requested ${zi32.v}')
+	}
+
+	return zi32.v
+}
 
 interface Identifiable {
 	id_string() string
